a9bf2d2f27a2375094812ad0fd5438563e430487
Scaccoman <scaccoman@gmail.com>
2017-12-20 09:00:16 +0000

    add cors package

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.static(__dirname + '/'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
