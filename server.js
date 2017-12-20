76647f0570725754aeaa279f7517bd8d29365738
Scaccoman <scaccoman@gmail.com>
2017-12-20 08:52:59 +0000

    modify express server to add cors

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(function(req, res, next) {
 });
 
 app.get('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
