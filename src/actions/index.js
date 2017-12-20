3e7362398486b5c37fe0e85eaec69ba0caa90410
Scaccoman <scaccoman@gmail.com>
2017-12-20 13:13:03 +0000

    add cors anywhere api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_KEY = "8634984bc2a88c463b4310b25f8057a1";
-export const SEARCH_URL = `https://food2fork.com/api/search?key=${API_KEY}`;
-export const GET_URL    = `https://food2fork.com/api/get?key=${API_KEY}`;
+export const SEARCH_URL = `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}`;
+export const GET_URL    = `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/get?key=${API_KEY}`;
 
 
 export const FETCH_RECIPES = "FETCH_RECIPES";
@@ -10,14 +10,8 @@ export const GET_RECIPE = "GET_RECIPE";
 
 export function fetchRecipes(searchTerm) {
     const url = `${SEARCH_URL}&q=${searchTerm}`;
-    const request = axios.get(url, 
-                    {
-                        mode: "cors",
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        }
-                    });
+    const request = axios.get(url);
+    
     return {
         type: FETCH_RECIPES,
         payload: request
