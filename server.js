d255af1789a86c488e282043661fb977f9367349
Scaccoman <scaccoman@gmail.com>
2017-12-20 13:02:12 +0000

    change cors config

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 
-app.use(cors({credentials: true, origin: true}));
+app.use(function (req, res, next) {
+
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', '*');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
+});
 app.use(express.static(__dirname + '/'));
 
 app.get('*', (req, res) => {
