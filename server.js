9eecd809f7e767b9ef0571318b460ac8916b76e6
Scaccoman <scaccoman@gmail.com>
2017-12-20 09:02:10 +0000

    change cors configuration

--- server.js
+++ server.js
@@ -4,19 +4,10 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.static(__dirname + '/'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-app.get('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
