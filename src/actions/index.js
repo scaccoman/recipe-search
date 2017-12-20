a899f88530a3eec5b58138335894465490989813
Scaccoman <scaccoman@gmail.com>
2017-12-20 13:17:13 +0000

    add cors api again

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_KEY = "8634984bc2a88c463b4310b25f8057a1";
-export const SEARCH_URL = `https://food2fork.com/api/search?key=${API_KEY}`;
-export const GET_URL    = `https://food2fork.com/api/get?key=${API_KEY}`;
+export const SEARCH_URL = `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}`;
+export const GET_URL    = `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/get?key=${API_KEY}`;
 
 
 export const FETCH_RECIPES = "FETCH_RECIPES";
